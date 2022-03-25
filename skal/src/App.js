1d8af10e58bd6983fcce738d69af148a81d2475f
theGrape1337 <thegrape1337@protonmail.com>
2022-03-25 21:12:07 +0100

    login form

--- skal/src/App.js
+++ skal/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          FRUITY GANG Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Fruity Gang at it again</h3>
+      <LoginForm  />
     </div>
   );
 }
