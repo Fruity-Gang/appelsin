4814c74510eb8c7e23e5544c05421bfbdf3f8d5d
theGrape1337 <thegrape1337@protonmail.com>
2022-03-25 22:31:14 +0100

    form submit

--- skal/src/App.js
+++ skal/src/App.js
@@ -4,11 +4,14 @@ import LoginForm from './components/LoginForm';
 
 function App() {
 
+  const login = (credentials) => {
+    console.log(credentials);
+  }
 
   return (
     <div className='container'>
       <h3>Fruity Gang at it again</h3>
-      <LoginForm  />
+      <LoginForm onLogin={login} />
     </div>
   );
 }
